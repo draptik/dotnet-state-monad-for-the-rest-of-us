b7e600889980bfc08967b507d282592d55b1ab41
Patrick Drechsler <patrick@pdrechsler.de>
2024-08-13 00:26:12 +0200

    ♻️ refactor: split Part2 into submodules and apply linting to all files

--- code/tests/Demo.MyLib.Tests/Part2.fs
+++ code/tests/Demo.MyLib.Tests/Part2.fs
@@ -5,77 +5,73 @@ module Part2 =
     open Swensen.Unquote
     open Xunit
 
-    // 1/3 =====================================================
+    module Part2_1 =
 
-    (*
-    type Tree<'a> =
-        | Leaf of 'a
-        | Node of Tree<'a> * Tree<'a>
+        type Tree<'a> =
+            | Leaf of 'a
+            | Node of Tree<'a> * Tree<'a>
 
-    let rec lengths = 
-        function
-        | Leaf v -> Leaf (String.length v)
-        | Node(l, r) -> Node(lengths l, lengths r)
+        let rec lengths =
+            function
+            | Leaf v -> Leaf(String.length v)
+            | Node(l, r) -> Node(lengths l, lengths r)
 
-    [<Fact>]
-    let ``calculate the leaves' content length`` () =
-        let treeOfWords = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
-        let treeOfNumbers = Node(Leaf 3, Node(Leaf 3, Leaf 5))
+        [<Fact>]
+        let ``calculate the leaves' content length`` () =
+            let treeOfWords = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+            let treeOfNumbers = Node(Leaf 3, Node(Leaf 3, Leaf 5))
 
-        let treeOfLengths = lengths treeOfWords
+            let treeOfLengths = lengths treeOfWords
 
-        test <@ treeOfLengths = treeOfNumbers @>
-    *)
+            test <@ treeOfLengths = treeOfNumbers @>
 
-    // 2/3 =====================================================
+    module Part2_2 =
 
-    (*
-    type Tree<'a> =
-        | Leaf of 'a
-        | Node of Tree<'a> * Tree<'a>
+        type Tree<'a> =
+            | Leaf of 'a
+            | Node of Tree<'a> * Tree<'a>
 
-    let (^+) l r = Node(l, r)
+        let (^+) l r = Node(l, r)
 
-    let rec lengths =
-        function
-        | Leaf v -> Leaf(String.length v)
-        | Node(l, r) -> lengths l ^+ lengths r
+        let rec lengths =
+            function
+            | Leaf v -> Leaf(String.length v)
+            | Node(l, r) -> lengths l ^+ lengths r
 
-    [<Fact>]
-    let ``calculate the leaves' content length`` () =
-        let treeOfWords = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
-        let treeOfNumbers = Node(Leaf 3, Node(Leaf 3, Leaf 5))
+        [<Fact>]
+        let ``calculate the leaves' content length`` () =
+            let treeOfWords = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+            let treeOfNumbers = Node(Leaf 3, Node(Leaf 3, Leaf 5))
 
-        let treeOfLengths = lengths treeOfWords
+            let treeOfLengths = lengths treeOfWords
 
-        test <@ treeOfLengths = treeOfNumbers @>
-    *)
+            test <@ treeOfLengths = treeOfNumbers @>
 
-    // 3/3 =====================================================
+    module Part2_3 =
 
-    type Tree<'a> =
-        | Leaf of 'a
-        | Node of Tree<'a> * Tree<'a>
+        type Tree<'a> =
+            | Leaf of 'a
+            | Node of Tree<'a> * Tree<'a>
 
-    let baseCase _ = 1
-    let baseCase' v = Leaf(String.length v)
-    let (^+) l r = Node(l, r)
+        let baseCase _ = 1
+        let baseCase' v = Leaf(String.length v)
+        let (^+) l r = Node(l, r)
 
-    let rec numberOfLeafs =
-        function
-        | Leaf v -> baseCase v
-        | Node(l, r) -> numberOfLeafs l + numberOfLeafs r
+        let rec numberOfLeafs =
+            function
+            | Leaf v -> baseCase v
+            | Node(l, r) -> numberOfLeafs l + numberOfLeafs r
 
-    let rec lengths =
-        function
-        | Leaf v -> baseCase' v
-        | Node(l, r) -> lengths l ^+ lengths r
+        let rec lengths =
+            function
+            | Leaf v -> baseCase' v
+            | Node(l, r) -> lengths l ^+ lengths r
 
-    [<Fact>]
-    let ``calculate the leaves' content length`` () =
-        let treeOfWords = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
-        let treeOfNumbers = Node(Leaf 3, Node(Leaf 3, Leaf 5))
+        [<Fact>]
+        let ``calculate the leaves' content length`` () =
+            let treeOfWords = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+            let treeOfNumbers = Node(Leaf 3, Node(Leaf 3, Leaf 5))
 
-        let treeOfLengths = lengths treeOfWords
+            let treeOfLengths = lengths treeOfWords
 
-        test <@ treeOfLengths = treeOfNumbers @>
+            test <@ treeOfLengths = treeOfNumbers @>
