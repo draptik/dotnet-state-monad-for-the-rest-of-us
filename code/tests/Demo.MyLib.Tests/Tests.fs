699d20a03b86b3ec3ddb391c8c9818a9d3d75dcd
Patrick Drechsler <patrick@pdrechsler.de>
2024-08-12 23:08:12 +0200

    ✨ feat: add example Unquote test

--- code/tests/Demo.MyLib.Tests/Tests.fs
+++ code/tests/Demo.MyLib.Tests/Tests.fs
@@ -3,6 +3,14 @@ module Tests
 open Swensen.Unquote
 open Xunit
 
+type Tree = Leaf | Node of (Tree * Tree)
+
+let numberOfLeaves tree = failwith "Not yet implemented"
+
 [<Fact>]
-let ``My test`` () =
-    test <@ true = true @>
+let ``counts the number of leaves in a tree`` () =
+    let treeWith3Leaves = failwith "Not yet implemented"
+    
+    let leaves = numberOfLeaves treeWith3Leaves
+    
+    test <@ leaves = 3 @>
