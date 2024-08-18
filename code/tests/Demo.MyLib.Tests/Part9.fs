4d4d67c57ecf9ee671c7735fa40eb42d3364951d
Patrick Drechsler <patrick@pdrechsler.de>
2024-08-18 22:53:39 +0200

    ✨ feat: Part 9 - keep buildLeaf simple by adding new function '<*'

--- code/tests/Demo.MyLib.Tests/Part9.fs
+++ code/tests/Demo.MyLib.Tests/Part9.fs
@@ -24,7 +24,7 @@ module Part9 =
         let run (WithCount f) (count: int) = f count
 
         let buildNode l r = Node(l, r)
-        
+
         let buildLeaf v count _ = Leaf(v, count)
 
         let pure' v = WithCount(fun count -> (v, count))
@@ -36,13 +36,54 @@ module Part9 =
                 let b = fv av
                 b, ac)
 
-        let getCount = WithCount (fun count -> (count, count))
-        
+        let getCount = WithCount(fun count -> (count, count))
+
         let incrementCount = WithCount(fun count -> ((), count + 1))
-        
+
         let rec index =
             function
             | Leaf v -> pure' buildLeaf <*> pure' v <*> getCount <*> incrementCount
+            | Node(l, r) -> pure' buildNode <*> index l <*> index r
+
+        [<Fact>]
+        let ``indexes a tree`` () =
+            let withCount = index tree
+            let indexed, _ = run withCount 1
+
+            test <@ indexed = Node(Leaf("one", 1), Node(Leaf("two", 2), Leaf("three", 3))) @>
+
+    module Part9_2 =
+
+        type WithCount<'v> = WithCount of (int -> 'v * int)
+
+        let run (WithCount f) (count: int) = f count
+
+        let buildNode l r = Node(l, r)
+
+        let buildLeaf v count = Leaf(v, count)
+
+        let pure' v = WithCount(fun count -> (v, count))
+
+        let (<*>) f a =
+            WithCount(fun count ->
+                let fv, fc = run f count
+                let av, ac = run a fc
+                let b = fv av
+                b, ac)
+
+        let (<*) f v =
+            WithCount(fun count ->
+                let fv, fc = run f count
+                let _, newCount = run v fc
+                (fv, newCount))
+            
+        let getCount = WithCount(fun count -> (count, count))
+
+        let incrementCount = WithCount(fun count -> ((), count + 1))
+
+        let rec index =
+            function
+            | Leaf v -> pure' buildLeaf <*> pure' v <*> getCount <* incrementCount
             | Node(l, r) -> pure' buildNode <*> index l <*> index r
 
         [<Fact>]
