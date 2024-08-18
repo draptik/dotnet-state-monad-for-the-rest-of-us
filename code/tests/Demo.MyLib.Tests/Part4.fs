656808b0ba1a2bb42cc47b2d690ea56340485243
Patrick Drechsler <patrick@pdrechsler.de>
2024-08-18 21:09:33 +0200

    ✨ feat: Part 4 - impure solution

---
+++ code/tests/Demo.MyLib.Tests/Part4.fs
@@ -0,0 +1,31 @@
+namespace Demo.MyLib.Tests
+
+module Part4 =
+
+    open Swensen.Unquote
+    open Xunit
+
+    type Tree<'a> =
+        | Leaf of 'a
+        | Node of Tree<'a> * Tree<'a>
+
+    let rec map f =
+        fun tree ->
+            match tree with
+            | Leaf v -> Leaf(f v)
+            | Node(l, r) -> Node(map f l, map f r)
+
+    module Part4_1 =
+
+        let mutable counter = 1
+
+        let impureIndex v =
+            let indeedLeaf = (v, counter)
+            counter <- counter + 1
+            indeedLeaf
+
+        [<Fact>]
+        let ``indexes a tree - impure`` () =
+            let tree = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+            let indexed = map impureIndex tree
+            test <@ indexed = Node(Leaf("one", 1), Node(Leaf("two", 2), Leaf("three", 3))) @>
